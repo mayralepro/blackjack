56c1f10000d9ddebeee2b46f9cc894136fac9d6e
Mayra L <123515252+mayralepro@users.noreply.github.com>
2023-06-11 22:50:15 -0700

    added files

---
+++ js/practice.js
@@ -0,0 +1,16 @@
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let airbnbCastle = {
+  name: "Live like a King in my Castle",
+  guests: 4,
+  bedroom: 1,
+  beds: 2,
+  bath: ["private", "half-bath"],
+  superhost: true,
+  price: 190,
+};
+
+console.log(airbnbCastle.name);
+console.log(airbnbCastle.bath[0]);
